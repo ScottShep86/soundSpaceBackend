ac9b5a501cc019e9f543af78811f89a8b3c850e8
Scott Shepherd <scottshep86@gmail.com>
2023-05-18 23:03:09 +0200

    started models

---
+++ soundSpace/models/Review.model.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const reviewSchema = new Schema(
+  {
+    createdBy: [
+      {
+        type: [Schema.Types.ObjectId],
+        ref: "Producer", // Reference to the "Producer" model
+      },
+      {
+        type: [Schema.Types.ObjectId],
+        ref: "RecordLabel", // Reference to the "RecordLabel" model
+      },
+    ],
+    comment: {
+      type: String,
+      required: true,
+    },
+    rating: { type: Number, required: true },
+
+    created: { type: Date, default: Date.now },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Review = model("Review", reviewSchema);
+
+module.exports = Review;
