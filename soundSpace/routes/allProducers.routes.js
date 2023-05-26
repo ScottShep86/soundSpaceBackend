87dd726fdf3ccf8b3ccb4bd06db5c1873363f796
Scott Shepherd <scottshep86@gmail.com>
2023-05-26 14:54:07 +0200

    filter on AllProducers

--- soundSpace/routes/allProducers.routes.js
+++ soundSpace/routes/allProducers.routes.js
@@ -3,33 +3,34 @@ const router = require("express").Router();
 const Producer = require("../models/Producer.model");
 
 //GET all the producers
-router.get("/", async (req, res, next) => {
+/* router.get("/", async (req, res, next) => {
   try {
     const allProducers = await Producer.find();
     res.status(200).json(allProducers);
   } catch (error) {
     console.error(error);
   }
-});
+}); */
 
-/* router.get("/producers", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   const { search } = req.query
   try {
       let filter = {}
-      if(searchName) {
-          filter = {name: {$regex: searchName, $option: "i"}}
-      }
+      if(search) {
+          filter = {name: {$regex: search, $options: "i"}}
+  /*     }
       if(searchLocation) {
           filter = {location: {$regex: search, $option: "i"}}
       }
       if(searchGenre) {
-          filter = {genre: {$regex: search, $option: "i"}}
+          filter = {genre: {$regex: search, $option: "i"}} */
       }
+      console.log(filter)
       const allProducers = await Producer.find(filter);
       res.status(200).json(allProducers);
   } catch (error) {
       console.error(error)
   }
-}) */
+})
 
 module.exports = router;
